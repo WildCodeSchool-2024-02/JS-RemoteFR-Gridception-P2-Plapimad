b1a813624e6fb66bc070ad94b2fbf8391b83d3a7
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-04-10 12:12:12 +0200

    Add Accueil component and logo Plapimad in image (but it doesn't work)

---
+++ client/src/components/Accueil.jsx
@@ -0,0 +1,22 @@
+function Accueil() {
+
+    return (
+        <>
+            <div className="nav_bar">
+                <img src="../assets/images/Logo_Plapimad.png" alt="Logo Plapimad" />
+                <ul>
+                    <li>Activités</li>
+                    <li>Contact</li>
+                </ul>
+                <div className="logos">
+                    <img src="" alt="logo GitHub" />
+
+
+                </div>
+            </div>
+            <h1>AventuresAtmo</h1>
+            <p>Chaque prévisions est une opportunité à Tours</p>
+        </>
+    )
+}
+export default Accueil;
