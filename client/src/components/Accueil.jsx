b6956de2cfdd55150ac9c4e677bb157d2780d930
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-04-10 22:23:41 +0200

    Add background-image in accueil.scss, change size and color of p and h1

--- client/src/components/Accueil.jsx
+++ client/src/components/Accueil.jsx
@@ -1,9 +1,9 @@
 import "../scss/accueil.scss";
 function Accueil() {
   return (
-    <>
+    <div className="global-div">
       <div className="logos_bar">
-        <img src="../assets/images/Logo_Plapimad.png" alt="Logo Plapimad" />
+        <img src="./assets/images/Logo_Plapimad.png" alt="Logo Plapimad" />
 
         <div className="logos">
           <img src="./assets/images/logo_GitHub.jpg" alt="Logo GitHub" />
@@ -24,7 +24,7 @@ function Accueil() {
         <h1>AventuresAtmo</h1>
         <p>Chaque prévisions est une opportunité à Tours</p>
       </div>
-    </>
+    </div>
   );
 }
 export default Accueil;
