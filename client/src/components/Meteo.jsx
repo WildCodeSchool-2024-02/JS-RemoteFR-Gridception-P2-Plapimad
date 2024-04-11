5f0e7c31e5be5e6281201dbdccafacfd711d41f5
pierre delarocque <delarocque.pierre@gmail.com>
2024-04-11 15:07:08 +0200

    end scss

--- client/src/components/Meteo.jsx
+++ client/src/components/Meteo.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import '../scss/Meteo.scss'
+import "../scss/Meteo.scss";
 
 function Meteo() {
   const [datas, setDatas] = useState({});
@@ -20,30 +20,28 @@ function Meteo() {
   }, []);
 
   return (
-    <>
     <div className="meteo-container">
       <div className="box-info">
-      <h1>Méteo</h1>
-      <h2>{datas?.name}</h2>
+        <h1>Méteo</h1>
+        <h2>{datas?.name}</h2>
       </div>
       <div className="box-globale">
-      <div className="box-temp">
-      <p>Température: {datas?.main?.temp}°</p>
-      <p>Ressenti: {datas?.main?.feels_like}°</p>
-      </div>
-      {datas?.weather?.[0]?.icon && (
-        <img
-        src={`https://openweathermap.org/img/wn/${datas.weather[0].icon}@2x.png`}
-        alt="petite incon"
-        />
+        <div className="box-temp">
+          <p>Température: {datas?.main?.temp}°</p>
+          <p>Ressenti: {datas?.main?.feels_like}°</p>
+        </div>
+        {datas?.weather?.[0]?.icon && (
+          <img
+            src={`https://openweathermap.org/img/wn/${datas.weather[0].icon}@2x.png`}
+            alt="petite incon"
+          />
         )}
-      <div className="icon-description">
-        <p>{datas?.weather?.[0]?.description}</p>
-        <p>💨 {datas?.wind?.speed} m/s</p>
-      </div>
+        <div className="icon-description">
+          <p>{datas?.weather?.[0]?.description}</p>
+          <p>💨 {datas?.wind?.speed} m/s</p>
+        </div>
       </div>
     </div>
-    </>
   );
 }
 export default Meteo;
