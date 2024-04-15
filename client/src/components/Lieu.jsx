15c616e44771c3f0ebb4dc26657f3cfd5857d9bd
Lauryn MARTIN <lauryn.martin333@gmail.com>
2024-04-15 16:54:49 +0200

    Add carrousel V1

--- client/src/components/Lieu.jsx
+++ client/src/components/Lieu.jsx
@@ -1,16 +1,27 @@
-import ChateauC from "../assets/images/Chateau-chenonceau.jpg";
-import Festival from "../assets/images/terres-du-son.avif";
-import EscapeGame from "../assets/images/EscapeGame.jpeg";
-import Shopping from "../assets/images/Shopping.jpeg";
-import DomaineViticole from "../assets/images/Domaine-viticole.jpeg";
-import PlacePlumereau from "../assets/images/Place-Plumereau.jpeg";
 import "../scss/Lieu.scss";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
-function Lieu({ activitesListe }) {
+function Lieu() {
   return (
-    <div>
+    <>
       <h2>Les activités bon plans </h2>
-      <div className="lieu-container">
+      <Carousel>
+        {activitesListe.map((slide) => (
+          <div key={slide.Name}>
+            <img src={slide.Image} alt={slide.Name} />
+            <div className="overlay">
+              <h2 className="overlay_title">{slide.Name}</h2>
+              <p className="overlay_text">{slide.Description}</p>
+            </div>
+          </div>
+        ))}
+      </Carousel>
+    </>
+  );
+}
+
+/* <div className="lieu-container">
         <a
           className="box-activite1"
           href="https://www.chenonceau.com"
@@ -23,46 +34,43 @@ function Lieu({ activitesListe }) {
         <a href="https://www.terresduson.com" target="_blank" rel="noreferrer">
           <img src={Festival} alt="Imga festival" />
         </a>
-      </div>
-    </div>
-  );
-}
+      </div> */
 
 const activitesListe = [
   {
     Name: "Château de Chenonceau",
     Description: "Visite du Château ainsi que de ces jardins.",
     Lien: "https://www.chenonceau.com/",
-    Meteo: "Soleil / Pluie",
-    Image: { ChateauC },
+    Image:
+      "https://cdn.pixabay.com/photo/2020/06/27/07/15/chenonceau-5344916_1280.jpg",
   },
   {
     Name: "Place Plumereau Tours",
     Description: "Visite du vieux Tours et des quartiers historiques.",
     Lien: "https://www.touraineloirevalley.com/vieux-tours-place-plumereau-quartiers-historiques-bien-vivants/",
-    Meteo: "Soleil / Pluie",
-    Image: { PlacePlumereau },
+    Image:
+      "https://reservation.tours-tourisme.fr/medias/images/prestations/visite-guidee-le-vieux-tours-place-plumereau-23813.jpg",
   },
   {
     Name: "Château Gaudrelle",
     Description: "Visite de domaines viticoles.",
     Lien: "https://www.touraineloirevalley.com/producteurs-locaux/chateau-gaudrelle-rochecorbon/",
-    Meteo: "Soleil / Pluie",
-    Image: { DomaineViticole },
+    Image:
+      "https://www.enpaysdelaloire.com/sites/default/files/styles/ogimage/public/mediatheque/images/sem_0015558_MD.jpg?itok=nR1dCcT2",
   },
   {
     Name: "Rue Natioanale de Tours",
     Description: "Un moment shopping en ville.",
     Lien: "https://www.tripadvisor.fr/Attraction_Review-g187130-d246693-Reviews-Rue_Nationale-Tours_Indre_et_Loire_Centre_Val_de_Loire.html",
-    Meteo: "Soleil /Pluie",
-    Image: { Shopping },
+    Image:
+      "https://tse4.mm.bing.net/th?id=OIP.LDNhF1tRNeRmehzDIKGoFwHaE8&pid=Api",
   },
   {
     Name: "Escape Game",
     Description: "Un moment ludique entre amis où en famille.",
     Lien: "https://escapetime-tours.fr/",
-    Meteo: "Pluie",
-    Image: { EscapeGame },
+    Image:
+      "https://www.paris-annuaire.com/design/uploads/2018/12/escape-game.jpg",
   },
 ];
 
