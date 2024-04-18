4b68339e5a35a8dc9ef524b86f79a322e79124be
Benoît Vandanjon <benoit@vandanjon.fr>
2024-04-18 17:53:36 +0200

    remove useless labels

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -8,37 +8,21 @@ function Contact() {
       </section>
 
       <form>
-        <ul className="contact-ul">
-          <li>
-            <label className="form-lastname" />
-            <input placeholder="Nom*" />
-          </li>
-          <li>
-            <label className="form-name" />
-            <input placeholder="Prénom*" />
-          </li>
-          <li>
-            <label className="form-mail" />
-            <input placeholder="Mail*" />
-          </li>
-          <li>
-            <label className="form-message" />
-            <textarea
-              placeholder="Message*"
-              name="messagearea"
-              id="messagearea"
-              cols="30"
-              rows="10"
-            />
-          </li>
-          <li className="button-li">
-            <input
-              className="button"
-              type="submit"
-              value="Envoyez votre demande"
-            />
-          </li>
-        </ul>
+        <input placeholder="Nom*" />
+
+        <input placeholder="Prénom*" />
+
+        <input placeholder="Mail*" />
+
+        <textarea
+          placeholder="Message*"
+          name="messagearea"
+          id="messagearea"
+          cols="30"
+          rows="10"
+        />
+
+        <input className="button" type="submit" value="Envoyez votre demande" />
       </form>
     </div>
   );
