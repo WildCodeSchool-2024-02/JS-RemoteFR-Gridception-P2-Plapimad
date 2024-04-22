b7c80ca231e731fc1631fcdc475b1b917facfbfa
pierre delarocque <delarocque.pierre@gmail.com>
2024-04-22 18:32:21 +0200

    restaurant-js

---
+++ client/src/js/Restaurants.js
@@ -0,0 +1,92 @@
+const restoListe = [
+  {
+    nom: "L'Opidom",
+    adresse: "4 quai de la Guignière, Fondettes, 37230, France",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.touraineloirevalley.com%2Frestaurants%2Flopidom-fondettes%2F&psig=AOvVaw0ECDrVcDmxcNAEQJBNsfsu&ust=1713883157329000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIi477eG1oUDFQAAAAAdAAAAABAE",
+    lien: "https://www.lopidom.fr/fr/",
+    description:
+      "L'objectif de ce créatif obsédé par le produit est de faire découvrir à ses convives ce que la nature fait de meilleur, tout en les surprenant avec des associations originales qui invitent au voyage",
+  },
+  {
+    nom: "L'Évidence",
+    adresse: "1 place des Marronniers, Montbazon, 37250, France",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.petitfute.com%2Fv10739-montbazon-37250%2Fc1165-restaurants%2Fc4-cuisine-francaise%2Fc117-gastronomique%2F1655006-l-evidence.html&psig=AOvVaw2eDeNiigk115gOg0qneZ1f&ust=1713883406968000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPiYlbGH1oUDFQAAAAAdAAAAABAJ",
+    lien: "https://www.restaurant-levidence.com",
+    description: "L'évidence cuisine",
+  },
+  {
+    nom: "Arbore-et-sens",
+    adresse: "22 Rue Balzac, 37600 Loches",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.indreavelo.fr%2Frestaurants%2Farbore-et-sens%2F&psig=AOvVaw1sDRrwKtKchrXMumjR_N9l&ust=1713883593148000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJDIzoeI1oUDFQAAAAAdAAAAABAE",
+    lien: "https://restaurant-arbore-et-sens.fr",
+    description:
+      "Propose une cuisine d’une extrême finesse qui fait appel à tous vos sens",
+  },
+
+  {
+    nom: "Château de Pray",
+    adresse: "rue du Cèdre, à Chargé, Amboise, 37530, France",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.in%2FLocationPhotoDirectLink-g187116-d2235598-i577930556-Chateau_de_Pray-Amboise_Indre_et_Loire_Centre_Val_de_Loire.html&psig=AOvVaw3BBUewhq3pJkiEYp_7E-j5&ust=1713883781006000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPDOxeeI1oUDFQAAAAAdAAAAABAx",
+    lien: "https://www.chateaudepray.fr",
+    description:
+      "Un château hôtel de charme et son restaurant gastronomique étoilé",
+  },
+  {
+    nom: "l’Auberge Pom’Poire",
+    adresse: "21 Rte de Valleres, 37190 Azay-le-Rideau",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.fr%2FHotel_Review-g187117-d1577058-Reviews-L_Auberge_Pom_Poire-Azay_le_Rideau_Indre_et_Loire_Centre_Val_de_Loire.html&psig=AOvVaw3DdYvPWGOx6MI7UyV5zbK2&ust=1713884559739000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCIDe-NSL1oUDFQAAAAAdAAAAABAE",
+    lien: "https://aubergepompoire.fr/accueil/",
+    description:
+      "Pour une pause à la campagne ou un instant gourmet, située sur le plateau fruitier de Touraine entre les coteaux d'Azay-le-Rideau et de Vallères, entre les vallées de l'Indre et de la Loire, l'Auberge Pom'Poire vous propose ses accueillantes chambres classées 3 étoiles et une table gastronomique récompensée par le guide Michelin.",
+  },
+  {
+    nom: "Vincent cuisinier de campagne",
+    adresse: "19 Rue de la Galottière, 37140 Coteaux-sur-Loire",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.aleou.fr%2Frestaurants-seminaire%2F39303-vincent-cuisinier-de-campagne.html&psig=AOvVaw1ht7rx58eUm86Gx4RiM53t&ust=1713884643114000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCOC7-P6L1oUDFQAAAAAdAAAAABAE",
+    lien: "https://vincentcuisinierdecampagne.blogspot.com",
+    description:
+      "Un charme fou à partager sans modération... Ici, tout est exact, harmonieux et serein",
+  },
+  {
+    nom: "Auberge du Bon Laboureur",
+    adresse: "6 Rue Bretonneau, 37150 Chenonceaux",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.fr%2FRestaurant_Review-g187123-d1840209-Reviews-Auberge_du_Bon_Laboureur-Chenonceaux_Indre_et_Loire_Centre_Val_de_Loire.html&psig=AOvVaw1ldYvtv0rW1LMH20hUb7Yg&ust=1713884716013000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCuvp-M1oUDFQAAAAAdAAAAABAE",
+    lien: "https://www.bonlaboureur.com/fr/hotel-chenonceaux",
+    description: "LE CHARME D'UNE MAISON D'HÔTE",
+  },
+  {
+    nom: "Les Hautes Roches",
+    adresse: "86 quai de la Loire 37210 ROCHECORBON",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.fr%2FRestaurant_Review-g1060694-d27168420-Reviews-Les_Hautes_Roches_Restaurant_Gastronomique-Rochecorbon_Indre_et_Loire_Centre_Va.html&psig=AOvVaw3-s3JsWps1ZvdhsMnuV_xE&ust=1713884824507000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCKD83eiM1oUDFQAAAAAdAAAAABAE",
+    lien: "https://www.leshautesroches.com/fr/",
+    description:
+      "Taillées dans la roche, les chambres troglodytiques vous invitent à passer un séjour atypique au sein de notre luxueux hôtel restaurant d’Indre-et-Loire.",
+  },
+  {
+    nom: "La Tratt",
+    adresse: "94 Rue du Commerce, 37000 Tours",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.tripadvisor.fr%2FRestaurant_Review-g187130-d24178275-Reviews-La_Tratt-Tours_Indre_et_Loire_Centre_Val_de_Loire.html&psig=AOvVaw2c7r6OBLVxUZhiAp1FAMUU&ust=1713884030604000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNDMhtqJ1oUDFQAAAAAdAAAAABAJ",
+    lien: "https://www.tripadvisor.fr/Restaurant_Review-g187130-d24178275-Reviews-La_Tratt-Tours_Indre_et_Loire_Centre_Val_de_Loire.html",
+    description:
+      "Des pizzas à emporter avec un maximum de produits locaux, Vice Champion de France !",
+  },
+  {
+    nom: "LE TATOUE TOQUE",
+    adresse: "31 Rue Briçonnet, 37000 Tours",
+    image:
+      "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thefork.fr%2Frestaurant%2Fle-tatoue-toque-tours-nord-r807793&psig=AOvVaw1XiGDm_TtOru96yfx5CUK_&ust=1713884243941000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLiwzsOK1oUDFQAAAAAdAAAAABBN",
+    lien: "https://www.tatouetoque.fr",
+    description:
+      "Un Chef un peu toqué Champion du monde de Burgers, une ambiance chaleureuse, et une cuisine maison et authentique, c'est tout l'esprit du Tatoué Toqué!",
+  },
+];
+export default restoListe;
