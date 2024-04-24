564db11959783e1ecabd962bdd8fb8bc656409ed
MaevaBriolais <briolais.maeva.pro@gmail.com>
2024-04-24 15:18:44 +0200

    Affichage d'un message de confirmation à l'envoi d'un formulaire

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -2,55 +2,104 @@ import { useState } from "react";
 import "../scss/contact.scss";
 import Popup from "./Popup";
 
-
 function Contact() {
-const [showPopup, setShowPopup] = useState(false);
+  const [showPopup, setShowPopup] = useState(false);
 
-const togglePopup = (() => {
-  setShowPopup(!showPopup);  
-});
-const togglePopupClose = (() => {
-  setShowPopup(showPopup);
-  if (!showPopup){
+  const [lastName, setLastName] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleChangeFirstName = (event) => {
+    if (event.target.value !== null) {
+      setFirstName(event.target.value);
+    }
+  };
+
+  const handleChangeLastName = (event) => {
+    if (event.target.value !== null) {
+      setLastName(event.target.value);
+    }
+  };
+
+  const handleChangeEmail = (event) => {
+    if (event.target.value !== null) {
+      setEmail(event.target.value);
+    }
+  };
+
+  const handleChangeMessage = (event) => {
+    if (event.target.value !== null) {
+      setMessage(event.target.value);
+    }
+  };
+
+  const togglePopup = () => {
+    if (firstName !== "" && email !== "" && lastName !== "" && message !== "") {
+      setShowPopup(true);
+    }
+  };
+
+  const togglePopupClose = () => {
+    setShowPopup(false);
     window.location.reload();
-  }
-})
- 
+  };
+
   return (
     <div>
       <section className="backgroundimage" id="backgroundimage">
         <h1 className="titleform">Une idée à suggérer ? </h1>
       </section>
 
-      <form>
-        <input className="nom-container" required placeholder="Nom*" />
+      <form onSubmit={(event) => event.preventDefault()}>
+        <input
+          className="nom-container"
+          required
+          placeholder="Nom*"
+          value={lastName}
+          onChange={handleChangeLastName}
+        />
 
-        <input required placeholder="Prénom*" />
+        <input
+          required
+          placeholder="Prénom*"
+          value={firstName}
+          onChange={handleChangeFirstName}
+        />
 
-        <input className="mail-container" required placeholder="Mail*" />
+        <input
+          className="mail-container"
+          required
+          placeholder="Mail*"
+          value={email}
+          onChange={handleChangeEmail}
+        />
 
-        <textarea required
+        <textarea
+          required
           className="textarea-container"
           placeholder="Message*"
           name="messagearea"
           id="messagearea"
           cols="30"
           rows="10"
+          value={message}
+          onChange={handleChangeMessage}
         />
-      
-    <input className="button" onClick={togglePopup} value="Envoyez votre demande" />
-    
-{/* Afficher Popup si tous les champs sont renseignés */}
-    
-    {showPopup && (
-    <div className="popupContainer">
-      <Popup />
-      <button className="closePopup" onClick={togglePopupClose}>Confirmer</button>
-    </div>
-  )}
+
+        <button className="button" onClick={togglePopup}>
+          Envoyez votre demande
+        </button>
+
+        {showPopup && (
+          <div className="popupContainer">
+            <Popup />
+            <button className="closePopup" onClick={togglePopupClose}>
+              Confirmer
+            </button>
+          </div>
+        )}
       </form>
-
-
     </div>
   );
 }
