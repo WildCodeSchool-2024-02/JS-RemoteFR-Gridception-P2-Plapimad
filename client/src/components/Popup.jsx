3b58a47e560629b5c952ed63f90427e8763016ad
pierre delarocque <delarocque.pierre@gmail.com>
2024-04-29 15:27:46 +0200

    reparation 2

--- client/src/components/Popup.jsx
+++ client/src/components/Popup.jsx
@@ -1,13 +1,13 @@
 import "../scss/contact.scss";
 
 function Popup() {
-
-
-return (
-    <div className="Popup"> 
-        <p>Votre demande a bien été traitée. Nous reviendrons vers vous dans les plus brefs délais.</p>
-
+  return (
+    <div className="Popup">
+      <p>
+        Votre demande a bien été traitée. Nous reviendrons vers vous dans les
+        plus brefs délais.
+      </p>
     </div>
-)
+  );
 }
- export default Popup;
+export default Popup;
